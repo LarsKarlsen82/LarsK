e0944f66c0f79e934cb5df35bf3d76c83b952e0d
LarsKarlsen82 <lpuk82@gmail.com>
2024-02-29 08:54:49 +0100

    update

---
+++ src/Pages/NoPage/NoPage.jsx
@@ -0,0 +1,20 @@
+// NoPage.jsx
+import React from 'react';
+import errorGif from '../../Assets/images/Error_404.gif';
+
+const NoPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        404 - Page Not Found
+      </h1>
+      <img
+        src={errorGif}
+        alt="404 Not Found"
+        className="max-w-full h-auto"
+      />
+    </div>
+  );
+};
+
+export default NoPage;
