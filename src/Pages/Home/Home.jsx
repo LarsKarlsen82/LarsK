5c9e34c8372a719dcbbb70efe9ddcf81700d353e
LarsKarlsen82 <lpuk82@gmail.com>
2024-02-29 10:05:02 +0100

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,48 @@
+// import React, { useState } from 'react';
+
+// const Home = () => {
+//   const [smileyPosition, setSmileyPosition] = useState(0);
+
+//   const moveSmiley = () => {
+//     setSmileyPosition((prevPosition) => prevPosition + 50); // Adjust the value based on your design
+//   };
+
+//   return (
+//     <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 min-h-screen flex flex-col items-center justify-center">
+//       <div className="text-white text-center">
+//         <h1 className="text-4xl font-bold mb-4">Velkommen til min side</h1>
+//         <p className="text-lg">
+//           Denne Web App er for at vise hvad jeg kan med webudvikling, klik på NavLinks
+//         </p>
+//         <div className="mt-8">
+//           <a
+//             href="#"
+//             className="bg-white text-indigo-500 px-6 py-3 rounded-full hover:bg-indigo-500 hover:text-white transition duration-300"
+//             onClick={moveSmiley}
+//           >
+//             Get Started
+//           </a>
+//         </div>
+//         {/* Smiley Element with inline style for animation */}
+//         <div
+//           className="text-2xl mt-4"
+//           style={{
+//             transform: `translateX(${smileyPosition}px)`,
+//             transition: 'transform 1s ease-in-out',
+//           }}
+//         >
+//           😊
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
 import React, { useState } from 'react';
+import backgroundImage from '/images/NL.jpg'; 
 
 const Home = () => {
   const [smileyPosition, setSmileyPosition] = useState(0);
@@ -8,7 +52,15 @@ const Home = () => {
   };
 
   return (
-    <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 min-h-screen flex flex-col items-center justify-center">
+    <div
+      className="min-h-screen flex flex-col items-center justify-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <div className="text-white text-center">
         <h1 className="text-4xl font-bold mb-4">Velkommen til min side</h1>
         <p className="text-lg">
