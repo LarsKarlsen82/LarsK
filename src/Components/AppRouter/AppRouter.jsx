ef8cd58d6df14068ddcfc73ee9ab3cc398d3cc71
LarsKarlsen82 <lpuk82@gmail.com>
2024-03-19 16:40:34 +0100

    update beskrivelse

--- src/Components/AppRouter/AppRouter.jsx
+++ src/Components/AppRouter/AppRouter.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import About from '../../Pages/About/About';
 import Home from '../../Pages/Home/Home';
-import Customers from '../../Pages/Customers/Customers';
+import Customers from '../../Pages/Learn/Learn';
 import SignUp from '../../Pages/SignUp/SignUp';
 import CV from '../../Pages/CV/CV';
 import NoPage from '../../Pages/NoPage/NoPage';
@@ -15,7 +15,7 @@ const AppRouter = () => {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/customers" element={<Customers />} />
+        <Route path="/learn" element={<Learn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/cv" element={<CV />} />
         <Route path="*" element={<NoPage />} />
